e3346d834b6ad6056fac1b84aed2b714cf6d297c
swayam agarwal <you@example.com>
2025-08-29 12:31:39 +0530

    Added a get job controller

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -49,3 +49,36 @@ export const createJob = async (req, res) => {
     });
   }
 };
+
+
+export const getJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find().populate({
+      path: "company",
+      select: "name email industry website  employeeCount"
+      
+    });
+
+    if (!jobs || jobs.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No jobs found.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      count: jobs.length,
+      jobs,
+    });
+
+  } catch (error) {
+    console.error("Error fetching jobs:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server Error: Could not fetch jobs.",
+      error: error.message,
+    });
+  }
+};
+
